6166117e36c5d1742ea6c82daf7469c00699d7ff
lsb <leebutterman@gmail.com>
2023-07-06 02:58:46 -0700

    use embeddingdb.js

--- vector-search-sqlite-react/src/App.js
+++ vector-search-sqlite-react/src/App.js
@@ -3,109 +3,31 @@ import React from 'react';
 import { tableFromIPC } from 'apache-arrow';
 import codebk from './codewords.json';
 import { pipeline, env } from '@xenova/transformers';
-import * as ort from 'onnxruntime-web';
 import RangeSlider from 'react-bootstrap-range-slider';
 import {format} from 'd3-format';
-import {filteredTopKAsc, pqDist} from 'pq.js';
+import { distTopK, queryToTiledDist, makeONNXRunnables, flattenCodebook } from 'embeddingdb.js';
 
 const format3SI = format('.3s')
-const InferenceSession = ort.InferenceSession;
-const Tensor = ort.Tensor;
 
-const codebookshape = [codebk.length, codebk[0].length, codebk[0][0].length];
 // const pqD = 384;
 // const pqM = 48;
 // const pqDs = 8;
 // const pqK = 128;
-const codebkflat = Float32Array.from({length: codebk.length * codebk[0].length * codebk[0][0].length}, (e,i) => codebk[Math.floor(i / codebk[0].length / codebk[0][0].length)][Math.floor(i / codebk[0][0].length) % codebk[0].length][i % codebk[0][0].length])
-const codebkT = new Tensor("float32", codebkflat, codebookshape)
+
+const codebkflat = flattenCodebook(codebk);
 
 const numpyChunkSize = 200000;
-
-async function distTopK(inferenceSession, dists, filterColumn, filterValue, filterZero, filterShim, k) {
-  // console.log("trying to distTopk");
-  const {output: {data: topk}} = await inferenceSession.run({
-    "input": (new Tensor("float32", dists)),
-    "filterColumn": (new Tensor("float32", filterColumn)),
-    "filterValue": (new Tensor("float32", [filterValue])),
-    "filterZero": (new Tensor("float32", [filterZero])),
-    "filterShim": (new Tensor("float32", [filterShim])),
-    "k": (new Tensor("uint8", [k])),
-  });
-  return topk;
-}
-
-async function queryDist(inferenceSession, query, codebook, codebookShape, embeddings, embeddingTensorShape) {
-  // console.log("trying to querydist", {query, codebook, codebookShape, embeddings, embeddingTensorShape});
-  const {output: {data: distTile}} = await inferenceSession.run({
-    "query": (new Tensor("float32", query)),
-    "codebook": (new Tensor("float32", codebook, codebookShape)),
-    "embeddings": (new Tensor("uint8", embeddings, embeddingTensorShape)),
-
-  })
-  return distTile;
-}
-
-async function queryToTiledDist(query, embeddings, pqdistinf, dists, firstLetters, firstLetterInt, filteredtopkinf, k, intermediateValueFn, continueFn, embeddingCounter=0) {
-  // compute distances a tile at a time, update in the dists array, compute topk not more frequently than every 30 ms (to avoid excessive screen repainting)
-  // on an iphone, distances for 1M embeddings runs in ~100 ms whereas topk for 1M floats runs in ~3ms
-  // call a sentinel function to halt processing if the outside state changes (there is probably a better way)
-  const chunkSize = 100000;
-  let lastPaint = Date.now();
-  const maxTick = 30;
-  const timingStrings = [];
-  // console.log({dists: dists.length, chunkSize, firstLetters: firstLetters.length});
-  for(; embeddingCounter<embeddings.length; embeddingCounter++){
-    // console.log(`starting embedding ${embeddingCounter}`)
-    const {data: embeddingData, offset: embeddingOffset} = embeddings[embeddingCounter];
-    for(let i=0; i < (embeddingData.length / codebk.length) && continueFn(); i+=chunkSize) {
-      // console.log({i, embeddingDatalength: embeddingData.length, continuefn: continueFn()})
-      const startTime = Date.now();
-      const startEmbeddingPosition = i * codebk.length;
-      const embeddingTileLength = chunkSize * codebk.length;
-      const embeddingTensorShape = [chunkSize, codebk.length];
-      const embeddingTile = new Uint8Array(embeddingData.buffer, startEmbeddingPosition + embeddingData.byteOffset, embeddingTileLength);
-      // console.log({i, startEmbeddingPosition, embeddingTileLength, embeddingTensorShape, workingShape: [embeddings.length / codebk.length, codebk.length], embeddingTile, embeddings});
-      const distTile = await queryDist(pqdistinf, query, codebkflat, codebookshape, embeddingTile, embeddingTensorShape);
-      // console.log("got dists")
-      for(let j=0;j<chunkSize;j++) {
-        dists[embeddingOffset+i+j] = distTile[j];
-      }
-      // console.log("wrote dists");
-      const distTime = Date.now();
-      timingStrings.push(`${distTime - startTime}`)
-      // console.log("2")
-      intermediateValueFn({dists, distTime: timingStrings.join(), lastPaint});
-      // console.log("1")
-      if((i === 0 && embeddingCounter === 0) || (distTime - lastPaint > maxTick)) {
-        // console.log("0, or we're over time")
-        const topk = await distTopK(filteredtopkinf, dists, firstLetters, firstLetterInt, 0, 1024, k);
-        const topktime = Date.now();
-        timingStrings.push(`-${topktime-distTime} `);
-        intermediateValueFn({topk});
-        await (new Promise(r => setTimeout(r,0)));
-        lastPaint = Date.now()
-      }
-    }
-  }
-  if(continueFn()) {
-    const topk = await distTopK(filteredtopkinf, dists, firstLetters, firstLetterInt, 0, 1024, k);
-    intermediateValueFn({dists, distTime: timingStrings.join(), topk});
-  }
-}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {query: "where a word means like how it sounds", firstLetter: "", chunkCount: 10, k: 10, embeddings: [], dists: [], firstLetters: []};
     env.localModelPath = './models/' ;
-    env.backends.onnx.wasm.simd = true;
   }
   async componentDidMount() {
     let extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
     this.setState({extractor});
-    const filteredtopkinf = await InferenceSession.create(filteredTopKAsc);
-    const pqdistinf = await InferenceSession.create(pqDist, {executionProviders: ['wasm']});
+    const {filteredtopkinf, pqdistinf} = await makeONNXRunnables();
     this.setState({filteredtopkinf, pqdistinf}, () => this.loadEmbeddings(this.state.chunkCount));
   }
   async loadEmbeddings(maxCount) {
@@ -159,7 +81,7 @@ class App extends React.Component {
       return;
     }
     const embeddingCounter = onlyLast ? (embeddings.length - 1) : 0;
-    await queryToTiledDist(queryEmbedding, embeddings, pqdistinf, dists, firstLetters, firstLetterInt, filteredtopkinf, k, (h) => this.setState(h), () => query === this.state.query, embeddingCounter);
+    await queryToTiledDist(queryEmbedding, embeddings, codebk, codebkflat, pqdistinf, dists, firstLetters, firstLetterInt, filteredtopkinf, k, (h) => this.setState(h), () => query === this.state.query, embeddingCounter);
   }
   canPerformSearch () {
     const {extractor, embeddings} = this.state;
